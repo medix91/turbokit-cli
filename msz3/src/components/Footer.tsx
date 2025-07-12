52e30fedd25bc6a0550bf52af3cd928db47984b0
medix91 <mehdisalimzayani2@gmail.com>
2025-07-12 14:08:08 +0200

    fix: deleting test project folders

--- msz3/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 p-4 text-white mt-auto">
-      <p className="text-center">&copy; 2025 Turbokit</p>
-    </footer>
-  );
-}
