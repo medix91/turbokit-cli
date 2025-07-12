192741b0d3bc9041fd1e5a8df9fc907232bf42de
medix91 <mehdisalimzayani2@gmail.com>
2025-07-12 14:10:13 +0200

    fix: deleting test projects

--- msz2/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-3xl font-bold text-gray-900">Bienvenue dans Turbokit</h1>
-    </div>
-  );
-}
